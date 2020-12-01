53fecfe5f7d80f74b49e0cd1dd62da54e08438b6
Lammatian <mateusz.parafinski@gmail.com>
2020-12-01 23:30:21 +0000

    Update util
    Move the actual implementations to the .cpp file. Now to compile I also
    need to add ../util.cpp to g++.

---
+++ 2020/util.cpp
@@ -0,0 +1,23 @@
+#include "util.h"
+
+std::vector<std::string> readlines(std::ifstream& f) {
+    std::string line;
+    std::vector<std::string> result;
+
+    while (std::getline(f, line)) {
+        result.push_back(line);
+    }
+
+    return result;
+}
+
+std::vector<ll> readlls(std::ifstream& f) {
+    std::string line;
+    std::vector<ll> result;
+
+    while (std::getline(f, line)) {
+        result.push_back(std::stoll(line));
+    }
+
+    return result;
+}
--- 2020/util.h
+++ 2020/util.h
@@ -3,24 +3,5 @@
 
 typedef long long ll;
 
-std::vector<std::string> readlines(std::ifstream& f) {
-    std::string line;
-    std::vector<std::string> result;
-
-    while (std::getline(f, line)) {
-        result.push_back(line);
-    }
-
-    return result;
-}
-
-std::vector<ll> readlls(std::ifstream& f) {
-    std::string line;
-    std::vector<ll> result;
-
-    while (std::getline(f, line)) {
-        result.push_back(std::stoll(line));
-    }
-
-    return result;
-}
+std::vector<std::string> readlines(std::ifstream& f);
+std::vector<ll> readlls(std::ifstream& f);
