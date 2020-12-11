aec8ad6856cdf58b90a4228b7d6b1000f4210aa2
Lammatian <mateusz.parafinski@gmail.com>
2020-12-11 14:48:19 +0100

    Nicify day 11
    Make part 1 conform to the part 2 standard which is faster and more
    readable. Additionally we can generalise the solution method now and
    call the same method with just two different parameters to obtain
    solutions for both parts. Sexy.

--- 2020/11/sol.cpp
+++ 2020/11/sol.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <functional>
 
 #include "util.h"
 
@@ -13,24 +14,6 @@ struct point {
 typedef std::vector<point> VisibleSeats;
 typedef std::vector<std::vector<std::vector<point>>> VisibleSeatMap;
 
-bool in_bounds(int x, int y, int w, int h) {
-    return 0 <= x && x < w && 0 <= y && y < h;
-}
-
-std::vector<point> adjacent(int x, int y, int w, int h) {
-    std::vector<point> result;
-
-    for (int x_ = x - 1; x_ <= x + 1; ++x_) {
-        for (int y_ = y - 1; y_ <= y + 1; ++y_) {
-            if (in_bounds(x_, y_, w, h) && (x_ != x || y_ != y)) {
-                result.push_back({x_, y_});
-            }
-        }
-    }
-
-    return result;
-}
-
 void print_ferry(const Ferry& ferry) {
     std::cout << std::endl;
     for (auto& row: ferry) {
@@ -42,61 +25,70 @@ void print_ferry(const Ferry& ferry) {
     std::cout << std::endl;
 }
 
-bool update_seats(Ferry& ferry, OccupiedSeatCount& adj_count) {
+bool in_bounds(int x, int y, int w, int h) {
+    return 0 <= x && x < w && 0 <= y && y < h;
+}
+
+VisibleSeats get_adjacent_seats(const Ferry& ferry, int x, int y) {
+    VisibleSeats result;
     int h = ferry.size();
     int w = ferry[0].size();
+
+    for (int x_ = x - 1; x_ <= x + 1; ++x_) {
+        for (int y_ = y - 1; y_ <= y + 1; ++y_) {
+            if (x_ == x && y_ == y) {
+                continue;
+            }
+
+            if (in_bounds(x_, y_, w, h) && ferry[y][x] != '.') {
+                result.push_back({x_, y_});
+            }
+        }
+    }
+
+    return result;
+}
+
+int occupied_visible_seats(const Ferry& ferry,
+                           const VisibleSeatMap& visible_seats,
+                           int x,
+                           int y) {
+    int count = 0;
+
+    for (auto& p: visible_seats[y][x]) {
+        if (ferry[p.y][p.x] == '#') {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+bool update_seats(Ferry& ferry, VisibleSeatMap& visible_seats, int empty_threshold) {
     bool changed = false;
+    int h = ferry.size();
+    int w = ferry[0].size();
+
+    OccupiedSeatCount occ_count(h, std::vector<int>(w));
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            occ_count[y][x] = occupied_visible_seats(ferry, visible_seats, x, y);
+        }
+    }
 
     for (int y = 0; y < h; ++y) {
         for (int x = 0; x < w; ++x) {
-            if (ferry[y][x] == 'L' && adj_count[y][x] == 0) {
+            if (ferry[y][x] == 'L' && occ_count[y][x] == 0) {
                 changed = true;
                 ferry[y][x] = '#';
-            } else if (ferry[y][x] == '#' && adj_count[y][x] >= 4) {
+            } else if (ferry[y][x] == '#' && occ_count[y][x] >= empty_threshold) {
                 changed = true;
                 ferry[y][x] = 'L';
             }
         }
     }
 
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            int count = 0;
-            for (auto& p: adjacent(x, y, w, h)) {
-                if (ferry[p.y][p.x] == '#') {
-                    count++;
-                } 
-            }
-
-            adj_count[y][x] = count;
-        }
-    }
-
     return changed;
-}
-
-ll sol1(Ferry ferry) {
-    int h = ferry.size();
-    int w = ferry[0].size();
-    OccupiedSeatCount adj_count;
-
-    for (int i = 0; i < h; ++i) {
-        adj_count.push_back(std::vector<int>(w, 0));
-    }
-
-    while(update_seats(ferry, adj_count));
-
-    ll result = 0;
-
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            if (ferry[y][x] == '#') {
-                result++;
-            }
-        }
-    }
-
-    return result;
 }
 
 VisibleSeats get_visible_seats(const Ferry& ferry, int x, int y) {
@@ -126,65 +118,20 @@ VisibleSeats get_visible_seats(const Ferry& ferry, int x, int y) {
     return result;
 }
 
-int occupied_visible_seats(const Ferry& ferry,
-                           const VisibleSeatMap& visible_seats,
-                           int x,
-                           int y) {
-    int count = 0;
-
-    for (auto& p: visible_seats[y][x]) {
-        if (ferry[p.y][p.x] == '#') {
-            count++;
-        }
-    }
-
-    return count;
-}
-
-bool update_seats2(Ferry& ferry,
-                   const VisibleSeatMap& visible_seats) {
-    bool changed = false;
+ll generic_sol(Ferry ferry,
+               std::function<VisibleSeats (Ferry, int, int)> visible_seat_func,
+               int empty_threshold) {
     int h = ferry.size();
     int w = ferry[0].size();
-    OccupiedSeatCount occ_count;
+    VisibleSeatMap visible_seats(h, std::vector<VisibleSeats>(w));
 
     for (int y = 0; y < h; ++y) {
-        occ_count.push_back(std::vector<int>(w));
-
         for (int x = 0; x < w; ++x) {
-            occ_count[y][x] = occupied_visible_seats(ferry, visible_seats, x, y);
+            visible_seats[y][x] = visible_seat_func(ferry, x, y);
         }
     }
 
-    for (int x = 0; x < w; ++x) {
-        for (int y = 0; y < h; ++y) {
-            if (ferry[y][x] == '#' && occ_count[y][x] >= 5) {
-                changed = true;
-                ferry[y][x] = 'L';
-            } else if (ferry[y][x] == 'L' && occ_count[y][x] == 0) {
-                changed = true;
-                ferry[y][x] = '#';    
-            }
-        }
-    }
-
-    return changed;
-}
-
-ll sol2(Ferry ferry) {
-    int h = ferry.size();
-    int w = ferry[0].size();
-    VisibleSeatMap visible_seats;
-
-    for (int y = 0; y < h; ++y) {
-        visible_seats.push_back(std::vector<VisibleSeats>(w));
-
-        for (int x = 0; x < w; ++x) {
-            visible_seats[y][x] = get_visible_seats(ferry, x, y);
-        }
-    }
-
-    while (update_seats2(ferry, visible_seats));
+    while (update_seats(ferry, visible_seats, empty_threshold));
 
     ll result = 0;
 
@@ -205,8 +152,8 @@ int main(int argc, char** argv) {
 
     Ferry ferry = util::readlines(f);
 
-    std::cout << sol1(ferry) << std::endl;
-    std::cout << sol2(ferry) << std::endl;
+    std::cout << generic_sol(ferry, get_adjacent_seats, 4) << std::endl;
+    std::cout << generic_sol(ferry, get_visible_seats, 5) << std::endl;
 
     return 0;
 }
