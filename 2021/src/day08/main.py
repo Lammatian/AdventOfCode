0944abe49e44fe00b9ee5c5b60700e5bbdad15ca
Lammatian <mateusz.parafinski@gmail.com>
2021-12-08 06:59:42 +0100

    Prettify day 08
    This is actually so easy now.

--- 2021/src/day08/main.py
+++ 2021/src/day08/main.py
@@ -11,170 +11,39 @@ def part1(inp):
     return count
 
 
-inters = {
-    1: {
-        0: 2,
-        2: 1,
-        3: 2,
-        5: 1,
-        6: 1,
-        9: 2
-    },
-    0: {
-        2: 4,
-        3: 4,
-        5: 4,
-        6: 5,
-        9: 5
-    },
-    2: {
-        3: 4,
-        5: 3,
-        6: 4,
-        9: 4
-    },
-    3: {
-        5: 4,
-        6: 4,
-        9: 5
-    },
-    5: {
-        6: 5,
-        9: 5
-    }
-}
-
-
 def decode(line):
-    # XDDDD
-    codes = {}
-    revcodes = {}
-    iline = line[:]
+    p1 = next(seg for seg in line if len(seg) == 2)
+    line.remove(p1)
+    p7 = next(seg for seg in line if len(seg) == 3)
+    line.remove(p7)
+    p4 = next(seg for seg in line if len(seg) == 4)
+    line.remove(p4)
+    p8 = next(seg for seg in line if len(seg) == 7)
+    line.remove(p8)
+    p2 = next(seg for seg in line if len(set(p4).intersection(set(seg))) == 2)
+    line.remove(p2)
+    p5 = next(seg for seg in line if len(set(p2).intersection(set(seg))) == 3)
+    line.remove(p5)
+    p6 = next(seg for seg in line if len(set(p1).intersection(set(seg))) == 1)
+    line.remove(p6)
+    p9 = next(seg for seg in line if len(set(p4).intersection(set(seg))) == 4)
+    line.remove(p9)
+    p0 = next(seg for seg in line if len(set(p6).intersection(set(seg))) == 5)
+    line.remove(p0)
+    p3 = next(seg for seg in line if len(set(p6).intersection(set(seg))) == 4)
 
-    for i, seg in enumerate(iline):
-        if seg in codes:
-            continue
-        elif len(seg) == 2:
-            codes[seg] = 1
-            revcodes[1] = seg
-            continue
-        elif len(seg) == 3:
-            codes[seg] = 7
-            revcodes[7] = seg
-            continue
-        elif len(seg) == 4:
-            codes[seg] = 4
-            revcodes[4] = seg
-            continue
-        elif len(seg) == 7:
-            codes[seg] = 8
-            revcodes[8] = seg
-            continue
-        elif len(seg) == 5: #2, 3, 5
-            if 2 in revcodes and 3 in revcodes:
-                codes[seg] = 5
-                revcodes[5] = seg
-                continue
-            elif 2 in revcodes and 5 in revcodes:
-                codes[seg] = 3
-                revcodes[3] = seg
-                continue
-            elif 3 in revcodes and 5 in revcodes:
-                codes[seg] = 2
-                revcodes[2] = seg
-                continue
-
-            if 1 in revcodes:
-                if len(set(revcodes[1]).intersection(set(seg))) == 2:
-                    codes[seg] = 3
-                    revcodes[3] = seg
-                    continue
-            if 2 in revcodes:
-                if len(set(revcodes[2]).intersection(set(seg))) == 3:
-                    codes[seg] = 5
-                    revcodes[5] = seg
-                else:
-                    codes[seg] = 3
-                    revcodes[3] = seg
-                continue
-            if 4 in revcodes:
-                if len(set(revcodes[4]).intersection(set(seg))) == 2:
-                    codes[seg] = 2
-                    revcodes[2] = seg
-                    continue
-            if 5 in revcodes:
-                if len(set(revcodes[5]).intersection(set(seg))) == 3:
-                    codes[seg] = 2
-                    revcodes[2] = seg
-                else:
-                    codes[seg] = 3
-                    revcodes[3] = seg
-                continue
-            if 6 in revcodes:
-                if len(set(revcodes[6]).intersection(set(seg))) == 5:
-                    codes[seg] = 5
-                    revcodes[5] = seg
-                    continue
-            if 7 in revcodes:
-                if len(set(revcodes[7]).intersection(set(seg))) == 3:
-                    codes[seg] = 3
-                    revcodes[3] = seg
-                    continue
-            if 9 in revcodes:
-                if len(set(revcodes[9]).intersection(set(seg))) == 4:
-                    codes[seg] = 2
-                    revcodes[2] = seg
-                    continue
-        elif len(seg) == 6: #0, 6, 9
-            if 0 in revcodes and 6 in revcodes:
-                codes[seg] = 9
-                revcodes[9] = seg
-                continue
-            elif 0 in revcodes and 9 in revcodes:
-                codes[seg] = 6
-                revcodes[6] = seg
-                continue
-            elif 6 in revcodes and 9 in revcodes:
-                codes[seg] = 0
-                revcodes[0] = seg
-                continue
-
-            if 1 in revcodes:
-                if len(set(revcodes[1]).intersection(set(seg))) == 1:
-                    codes[seg] = 6
-                    revcodes[6] = seg
-                    continue
-            if 3 in revcodes:
-                if len(set(revcodes[3]).intersection(set(seg))) == 5:
-                    codes[seg] = 9
-                    revcodes[9] = seg
-                    continue
-            if 4 in revcodes:
-                if len(set(revcodes[4]).intersection(set(seg))) == 4:
-                    codes[seg] = 9
-                    revcodes[9] = seg
-                    continue
-            if 5 in revcodes:
-                if len(set(revcodes[5]).intersection(set(seg))) == 4:
-                    codes[seg] = 0
-                    revcodes[0] = seg
-                    continue
-            if 7 in revcodes:
-                if len(set(revcodes[7]).intersection(set(seg))) == 2:
-                    codes[seg] = 6
-                    revcodes[6] = seg
-                    continue
-            if 9 in revcodes:
-                if len(set(revcodes[9]).intersection(set(seg))) == 4:
-                    codes[seg] = 2
-                    revcodes[2] = seg
-                    continue
-
-        iline.append(seg)
-        if i == 1000:
-            exit(1)
-
-    return codes
+    return {
+        p0: 0,
+        p1: 1,
+        p2: 2,
+        p3: 3,
+        p4: 4,
+        p5: 5,
+        p6: 6,
+        p7: 7,
+        p8: 8,
+        p9: 9
+    }
 
 
 def part2(inp):
