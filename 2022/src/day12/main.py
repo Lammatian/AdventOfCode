2acfc69f45ee53f25d386f49176af80836cfc1f5
Lammatian <mateusz.parafinski@gmail.com>
2022-12-12 06:29:59 +0100

    Add day 12
    Part 1: 23:37 (1397th)
    Part 2: 27:29 (1302nd)
    
    One day I'll get back to decent ranks once again

---
+++ 2022/src/day12/main.py
@@ -0,0 +1,109 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+from itertools import product, permutations, combinations
+from functools import reduce
+from collections import Counter, defaultdict
+import numpy as np
+from queue import PriorityQueue, Queue
+from pyutils import *
+from copy import deepcopy
+
+
+def parse(line):
+    return list(line)
+
+
+def main():
+    if len(sys.argv) > 1:
+        filepath = f'{dir_path}/../../inputs/day12/' + sys.argv[1]
+    else:
+        filepath = f'{dir_path}/../../inputs/day12/input'
+
+    with open(filepath) as f:
+        inp = list(map(lambda x: parse(x), f.read().strip().split('\n')))
+
+    print(inp)
+
+    print(part1(deepcopy(inp)))
+    print(part2(deepcopy(inp)))
+
+
+def part1(inp):
+    seen = set()
+    R = len(inp)
+    C = len(inp[0])
+    q = PriorityQueue()
+    for r, row in enumerate(inp):
+        for c, val in enumerate(row):
+            if val == 'S':
+                q.put((0, r, c))
+                seen.add((r, c))
+                inp[r][c] = 'a'
+
+    while q:
+        steps, r, c = q.get()
+        D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for dr, dc in D:
+            rr = r + dr
+            cc = c + dc
+            if not (0 <= rr < R and 0 <= cc < C):
+                continue
+            if inp[r][c] in 'yz' and inp[rr][cc] == 'E':
+                return steps + 1
+            if not (ord(inp[rr][cc]) - ord(inp[r][c]) <= 1):
+                continue
+            if (rr, cc) in seen:
+                continue
+            q.put((steps + 1, rr, cc))
+            seen.add((rr, cc))
+
+
+def part2(inp):
+    starts = []
+    R = len(inp)
+    C = len(inp[0])
+    for r, row in enumerate(inp):
+        for c, val in enumerate(row):
+            if val == 'S':
+                inp[r][c] = 'a'
+    for r, row in enumerate(inp):
+        for c, val in enumerate(row):
+            if val in 'Sa':
+                starts.append((r, c))
+
+    best = 1e9
+    for sr, sc in starts:
+        seen = set()
+        q = PriorityQueue()
+        seen.add((sr, sc))
+        q.put((0, sr, sc))
+
+        done = False
+        qsize = 1
+        while not done and qsize > 0:
+            steps, r, c = q.get()
+            qsize -= 1
+            D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for dr, dc in D:
+                rr = r + dr
+                cc = c + dc
+                if not (0 <= rr < R and 0 <= cc < C):
+                    continue
+                if inp[r][c] in 'yz' and inp[rr][cc] == 'E':
+                    best = min(best, steps + 1)
+                    done = True
+                    break
+                if not (ord(inp[rr][cc]) - ord(inp[r][c]) <= 1):
+                    continue
+                if (rr, cc) in seen:
+                    continue
+                q.put((steps + 1, rr, cc))
+                qsize += 1
+                seen.add((rr, cc))
+    return best
+
+
+if __name__ == '__main__':
+    main()
+
