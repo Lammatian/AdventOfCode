549a01cdec697a36bb6a7f2c6541283c95d53830
Lammatian <mateusz.parafinski@gmail.com>
2022-12-13 06:33:12 +0100

    Add day 13
    Part 1: 22:24 (1217th)
    Part 2: 28:21 (1055th)
    
    At this point it's a fight to break into top 1000 😅

---
+++ 2022/src/day13/main.py
@@ -0,0 +1,81 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+from itertools import product, permutations, combinations
+from functools import reduce
+from collections import Counter, defaultdict
+import numpy as np
+from pyutils import *
+from copy import deepcopy
+from functools import cmp_to_key
+import ast
+
+
+def parse(line):
+    a, b = line.split('\n')
+    return [ast.literal_eval(a), ast.literal_eval(b)]
+
+
+def main():
+    if len(sys.argv) > 1:
+        filepath = f'{dir_path}/../../inputs/day13/' + sys.argv[1]
+    else:
+        filepath = f'{dir_path}/../../inputs/day13/input'
+
+    with open(filepath) as f:
+        inp = list(map(lambda x: parse(x), f.read().strip().split('\n\n')))
+
+    print(inp)
+
+    print(part1(deepcopy(inp)))
+    print(part2(deepcopy(inp)))
+
+
+def rec(x, y):
+    inright = True
+    if type(x) == type([]) and type(y) == type([]):
+        for x_, y_ in zip(x, y):
+            inright = rec(x_, y_)
+            if inright == -1:
+                return -1
+            if inright == 1:
+                return 1
+        return 1 if len(x) < len(y) else (0 if len(x) == len(y) else -1)
+    elif type(x) == type([]) and type(y) == type(5):
+        y = [y]
+        return rec(x, y)
+    elif type(x) == type(5) and type(y) == type([]):
+        x = [x]
+        return rec(x, y)
+    elif type(x) == type(5) and type(y) == type(5):
+        return 1 if x < y else (0 if x == y else -1)
+
+
+def inright(x, y):
+    res = rec(x, y)
+    return res == 1
+
+
+def part1(inp):
+    res = 0
+    for i, (a, b) in enumerate(inp):
+        if inright(a, b):
+            res += i + 1
+    return res
+
+
+def part2(inp):
+    packets = []
+    for a, b in inp:
+        packets.append(a)
+        packets.append(b)
+    packets.append([[2]])
+    packets.append([[6]])
+    packets = sorted(packets, key=cmp_to_key(rec), reverse=True)
+    return (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
+    
+
+
+if __name__ == '__main__':
+    main()
+
