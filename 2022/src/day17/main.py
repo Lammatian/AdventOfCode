ce23842e3ecc57a56c851e6c958b6ad960e1cbc0
Lammatian <mateusz.parafinski@gmail.com>
2022-12-17 09:49:47 +0100

    Add second solution to day 17
    Inspired by Jonathan Paulson, basically keep track of the set of all places
    with rocks. This is WAY easier to implement correctly for part 1 (especially
    the move logic), although was struggling a bit with part 2.

--- 2022/src/day17/main.py
+++ 2022/src/day17/main.py
@@ -24,8 +24,132 @@ def main():
 
     print(inp)
 
-    print(part1(deepcopy(inp)))
-    print(part2(deepcopy(inp)))
+    print(part1_set(deepcopy(inp)))
+    print(part2_set(deepcopy(inp)))
+    print(part1_grid(deepcopy(inp)))
+    print(part2_grid(deepcopy(inp)))
+
+
+def get_piece(s, y):
+    if s == 0:
+        return set([(2, y), (3, y), (4, y), (5, y)])
+    elif s == 1:
+        return set([(2, y+1), (3, y), (3, y+1), (3, y+2), (4, y+1)])
+    elif s == 2:
+        return set([(2, y), (3, y), (4, y), (4, y+1), (4, y+2)])
+    elif s == 3:
+        return set([(2, y), (2, y+1), (2, y+2), (2, y+3)])
+    elif s == 4:
+        return set([(2, y), (2, y+1), (3, y), (3, y+1)])
+    else:
+        assert False
+
+
+def move_right(piece):
+    if any(x+1 >= 7 for x, _ in piece):
+        return piece
+    return set([(x+1, y) for x, y in piece])
+
+
+def move_left(piece):
+    if any(x-1 < 0 for x, _ in piece):
+        return piece
+    return set([(x-1, y) for x, y in piece])
+
+
+def move_down(piece):
+    return set([(x, y-1) for x, y in piece])
+
+
+def move_up(piece):
+    return set([(x, y+1) for x, y in piece])
+
+
+def show(B):
+    maxy = max(y for x, y in B)
+    for y in range(maxy, 0, -1):
+        for x in range(7):
+            if (x, y) in B:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+
+def part1_set(inp):
+    B = set([(x, 0) for x in range(7)])
+    nm = 0
+    
+    for t in range(2022):
+        top = max(y for _, y in B) + 4
+        piece = get_piece(t%5, top)
+        while True:
+            if inp[nm] == '>':
+                piece = move_right(piece)
+                if piece & B:
+                    piece = move_left(piece)
+            elif inp[nm] == '<':
+                piece = move_left(piece)
+                if piece & B:
+                    piece = move_right(piece)
+            nm = (nm + 1) % len(inp)
+            piece = move_down(piece)
+            if piece & B:
+                piece = move_up(piece)
+                break
+        B |= piece
+
+    return max(y for _, y in B)
+
+
+def signature(B):
+    maxy = max(y for _, y in B)
+    return frozenset([(x, maxy-y) for x, y in B if maxy - y <= 6])
+
+
+def part2_set(inp):
+    B = set([(x, 0) for x in range(7)])
+    nm = 0
+    M = 1000000000000
+    
+    t = 0
+    seen = {}
+    top = 0
+    added_height = 0
+    added = False
+    while t < M:
+        piece = get_piece(t%5, top+4)
+        while True:
+            if inp[nm] == '>':
+                piece = move_right(piece)
+                if piece & B:
+                    piece = move_left(piece)
+            elif inp[nm] == '<':
+                piece = move_left(piece)
+                if piece & B:
+                    piece = move_right(piece)
+            nm = (nm + 1) % len(inp)
+            piece = move_down(piece)
+            if piece & B:
+                piece = move_up(piece)
+                B |= piece
+                top = max(y for _, y in B)
+                break
+
+        sig = (nm, t%5, signature(B))
+        if sig in seen and not added:
+            oldt, oldy = seen[sig]
+            dy = top - oldy
+            dt = t - oldt
+            amt = (M - t) // dt
+            added_height = dy * amt
+            t += dt * amt
+            added = True
+
+        seen[sig] = (t, top)
+        t += 1
+
+    return top + added_height
 
 
 shapes = {
@@ -62,7 +186,7 @@ def highest(board):
     return len(board)
 
 
-def part1(inp):
+def part1_grid(inp):
     global shapes
     board = [['.' for _ in range(7)] for _ in range(4)]
     names = ['minus', 'plus', 'l', 'I', 'square']
@@ -99,7 +223,7 @@ def top_rows(board):
     return rows
 
 
-def part2(inp):
+def part2_grid(inp):
     global shapes
     board = [['.' for _ in range(7)] for _ in range(4)]
     names = ['minus', 'plus', 'l', 'I', 'square']
