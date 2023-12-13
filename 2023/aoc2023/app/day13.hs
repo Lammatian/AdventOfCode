6c6aaf5cf27b61615d3b1e27e44e654c7ef29f51
Lammatian <mateusz.parafinski@gmail.com>
2023-12-13 21:57:27 +0100

    Prettify 2023 day 13
    * Removed unnecessary `find[Vertical|Horizontal]Reflection` methods, only using
      the ones that find all reflections.
    * Took care of the squiggles
    * Prettified/shortened main

--- 2023/aoc2023/app/day13.hs
+++ 2023/aoc2023/app/day13.hs
@@ -1,11 +1,5 @@
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
-import Data.Maybe (mapMaybe, isJust, fromJust)
-import Lib (printGrid)
--- TODO: find[Vertical|Horizontal]Reflection functions are not needed, use only plural
--- TODO: Use Reflection type everywhere
--- TODO: can write findReflections
--- TODO: take care of all the yellow squiggles
 
 type Board = [String]
 data ReflType = Vert | Hor deriving (Eq, Show)
@@ -22,12 +16,6 @@ isVerticalReflection b c = and compCols
     right = map (take refl . drop (c + 1)) b
     compCols = zipWith (==) left right
 
-findVerticalReflection :: Board -> Int -> Maybe Int
-findVerticalReflection b c
-  | c == length (head b) - 1 = Nothing
-  | isVerticalReflection b c = Just (c + 1)
-  | otherwise                = findVerticalReflection b (c + 1)
-
 findVerticalReflections :: Board -> Int -> [Reflection]
 findVerticalReflections b c
   | c == length (head b) - 1 = []
@@ -41,12 +29,6 @@ isHorizontalReflection b u d
   where
     areMatchingRows = (b !! u) == (b !! d)
 
-findHorizontalReflection :: Board -> Int -> Maybe Int
-findHorizontalReflection b r
-  | r == length b - 1                  = Nothing
-  | isHorizontalReflection b r (r + 1) = Just (r + 1)
-  | otherwise                          = findHorizontalReflection b (r + 1)
-
 findHorizontalReflections :: Board -> Int -> [Reflection]
 findHorizontalReflections b r
   | r == length b - 1                  = []
@@ -59,10 +41,13 @@ replace b r c = take r b ++ [newRow] ++ drop (r + 1) b
     row = b !! r
     swap '#' = '.'
     swap '.' = '#'
+    swap chr = error $ "Unexpected character in board: " ++ [chr]
     newRow = take c row ++ [swap (row !! c)] ++ drop (c + 1) row
 
+-- Brute force: try to replace each spot in the board and see if that produces a new reflection
+-- Another approach would be to count the number of differences in a reflection, but cba lol
 findNewReflection :: Board -> Reflection -> Reflection
-findNewReflection b rf@(R rt i) = go b 0 0 rf
+findNewReflection board = go board 0 0
   where
     go b r c rf
       | c >= length (head b) || r >= length b = error $ "Why did this happen " ++ show r ++ " " ++ show c
@@ -75,6 +60,15 @@ findNewReflection b rf@(R rt i) = go b 0 0 rf
         vertical = filter (/= rf) $ findVerticalReflections newBoard 0
         (newR, newC) = if c == length (head b) - 1 then (r + 1, 0) else (r, c + 1)
 
+findOriginalReflection :: Board -> Reflection
+findOriginalReflection b
+  | not (null horizontal) = head horizontal
+  | not (null vertical)   = head vertical
+  | otherwise             = error "This shouldn't happen"
+  where
+    horizontal = findHorizontalReflections b 0
+    vertical = findVerticalReflections b 0
+
 reflValue :: Reflection -> Int
 reflValue (R Vert x) = x
 reflValue (R Hor x) = 100 * x
@@ -85,11 +79,6 @@ main =
     args <- getArgs
     contents <- readFile $ if not (null args) then head args else "inputs/day13/input.txt"
     let boards = map lines $ splitOn "\n\n" contents
-    let cols = mapMaybe (`findVerticalReflection` 0) boards
-    let rows = mapMaybe (`findHorizontalReflection` 0) boards
-    print $ 100 * sum rows + sum cols
-    let boardVertRefs = [(b, R Vert (fromJust refl)) | b <- boards, let refl = findVerticalReflection b 0, isJust refl]
-    let boardHorRefs = [(b, R Hor (fromJust refl)) | b <- boards, let refl = findHorizontalReflection b 0, isJust refl]
-    let boardRefs = boardHorRefs ++ boardVertRefs
-    print $ sum $ map (reflValue . uncurry findNewReflection) boardRefs
-    printGrid (head boards)
+    let refls = [(b, findOriginalReflection b) | b <- boards]
+    print $ sum $ map (reflValue . snd) refls
+    print $ sum $ map (reflValue . uncurry findNewReflection) refls
