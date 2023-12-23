31aac2a555177243df7c6ddaa5f9547482819d4f
Lammatian <mateusz.parafinski@gmail.com>
2023-12-23 11:57:48 +0100

    Add 2023 day 23 part 2
    Slow af

--- 2023/aoc2023/app/day23.hs
+++ 2023/aoc2023/app/day23.hs
@@ -2,13 +2,13 @@ import System.Environment (getArgs)
 import Data.Sequence as Sq (Seq((:|>), Empty, (:<|)), (><), mapWithIndex, fromList)
 import Data.Set as St (Set, notMember, insert, empty)
 import Data.Map as M (Map, empty, fromList, (!))
-import Debug.Trace (trace)
 
 type Board = [String]
 type Point = (Int, Int)
 -- Edges of a graph
-type Graph = Map Point [(Point, Int)]
-type Nodes = [Point]
+type Node = Point
+type Graph = Map Node [(Node, Int)]
+type Nodes = [Node]
 
 at :: Board -> Point -> Char
 at b (r, c) = (b!!r)!!c
@@ -39,26 +39,6 @@ longest b t ((f, p, d):<|ss)
     c = b `at` p
     ns = mapWithIndex (\_ a -> (p, a, d + 1)) $ neighbours b f p
 
-neighbours2 :: Board -> Set Point -> Point -> Seq Point
-neighbours2 b seen (r, c) = Sq.fromList $ [n | n <- allDirs, inBounds n, n `notMember` seen, not (isWall n)]
-  where
-    allDirs = [(r, c + 1), (r + 1, c), (r, c - 1), (r - 1, c)]
-    isWall n = b `at` n == '#'
-    inBounds (nr, nc) = nr >= 0 && nr < length b && nc >= 0 && nc < length (head b)
-
--- DFS
-longest2 :: Board -> Point -> Point -> Set Point -> Int
-longest2 b t p seen
-  | p == t = length seen
-  | otherwise = case ns of
-      Empty -> 0
-      (n:<|Empty) -> longest2 b t n (insert p seen)
-      (n1:<|n2:<|Empty) -> maximum [longest2 b t n (insert p seen) | n <- [n1, n2]]
-      (n1:<|n2:<|n3:<|Empty) -> maximum [longest2 b t n (insert p seen) | n <- [n1, n2, n3]]
-      _ -> error "Too many neighbours lol"
-  where
-    ns = neighbours2 b seen p
-
 getNodes :: Board -> Point -> Point -> [Point]
 getNodes b s e = [(r, c) | r <- [0..rr - 1], c <- [0..cc - 1], notWall (r, c), isNode (r, c)] ++ [s, e]
   where
@@ -84,19 +64,19 @@ findNode b f p d ns
     -- Assume there's always just one, otherwise would be in node
     nextP = head $ [n | n <- simpleNeighbours b p, n /= f]
 
-findNeighbours :: Board -> Point -> Nodes -> [(Point, Int)]
-findNeighbours b s ns = [findNode b s n 1 ns | n <- simpleNeighbours b s]
+findNeighbourNodes :: Board -> Node -> Nodes -> [(Node, Int)]
+findNeighbourNodes b s ns = [findNode b s n 1 ns | n <- simpleNeighbours b s]
 
 buildGraph :: Board -> Nodes -> Graph
 buildGraph _ [] = M.empty
-buildGraph b ns = M.fromList [(n, findNeighbours b n ns) | n <- ns]
+buildGraph b ns = M.fromList [(n, findNeighbourNodes b n ns) | n <- ns]
 
-longestPath :: Graph -> Point -> Point -> Set Point -> Int
-longestPath g s e visited
-  | s == e    = 0
-  | otherwise = maximum $ 0:[d + longestPath g s n (insert n visited) | (n, d) <- g!e, n `notMember` visited]
+-- This is surprisingly slow given how small the graph is lol
+longestPath :: Graph -> Node -> Int -> Node -> Set Node -> Int
+longestPath g p d t seen
+  | p == t    = d
+  | otherwise = maximum $ 0:[longestPath g n (d + d') t (insert n seen) | (n, d') <- g!p, n `notMember` seen]
 
--- Part 2: Either DFS or construct a graph
 main :: IO ()
 main =
   do
@@ -106,10 +86,6 @@ main =
     let start = (0, 1)
     let end = (length grid - 1, length (head grid) - 2)
     print $ longest grid end (Empty:|>((-1, 0), start, 0))
-    -- print $ longest2 grid end start empty
     let nodes = getNodes grid start end
-    print nodes
     let graph = buildGraph grid nodes
-    print graph
-    print $ longestPath graph start end St.empty
-    -- print $ findNeighbours grid start nodes
+    print $ longestPath graph start 0 end St.empty
