d97eef4743fd0dbec0dc2cb1feab71c462a1cb93
Lammatian <mateusz.parafinski@gmail.com>
2023-12-23 12:14:40 +0100

    Polish 2023 day 23
    🇵🇱

--- 2023/aoc2023/app/day23.hs
+++ 2023/aoc2023/app/day23.hs
@@ -1,7 +1,7 @@
 import System.Environment (getArgs)
-import Data.Sequence as Sq (Seq((:|>), Empty, (:<|)), (><), mapWithIndex, fromList)
 import Data.Set as St (Set, notMember, insert, empty)
 import Data.Map as M (Map, empty, fromList, (!))
+import Data.Maybe (catMaybes)
 
 type Board = [String]
 type Point = (Int, Int)
@@ -20,25 +20,6 @@ forcedNext '<' (r, c) = (r, c - 1)
 forcedNext '^' (r, c) = (r - 1, c)
 forcedNext _ _ = error "Do not call me that"
 
-neighbours :: Board -> Point -> Point -> Seq Point
-neighbours b f p@(r, c) = Sq.fromList $ [n | n <- allDirs, inBounds n, n /= f, not (useless n), not (isWall n)]
-  where
-    allDirs = [(r, c + 1), (r + 1, c), (r, c - 1), (r - 1, c)]
-    isWall n = b `at` n == '#'
-    inBounds (nr, nc) = nr >= 0 && nr < length b && nc >= 0 && nc < length (head b)
-    useless n = b `at` n `elem` "<>^v" && forcedNext (b `at` n) n == p
-
--- BFS
-longest :: Board -> Point -> Seq (Point, Point, Int) -> Int
-longest _ _ Empty = 0
-longest b t ((f, p, d):<|ss)
-  | p == t          = max d $ longest b t ss
-  | c `elem` "<>^v" = longest b t (ss:|>(p, forcedNext c p, d + 1))
-  | otherwise       = longest b t (ss><ns)
-  where
-    c = b `at` p
-    ns = mapWithIndex (\_ a -> (p, a, d + 1)) $ neighbours b f p
-
 getNodes :: Board -> Point -> Point -> [Point]
 getNodes b s e = [(r, c) | r <- [0..rr - 1], c <- [0..cc - 1], notWall (r, c), isNode (r, c)] ++ [s, e]
   where
@@ -49,27 +30,29 @@ getNodes b s e = [(r, c) | r <- [0..rr - 1], c <- [0..cc - 1], notWall (r, c), i
     ns (r, c) = filter (\p -> inBounds p && notWall p) [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]
     isNode p = length (ns p) > 2
 
-simpleNeighbours :: Board -> Point -> [Point]
-simpleNeighbours b (r, c) = [n | n <- allDirs, inBounds n, not (isWall n)]
+neighbours :: Board -> Bool -> Point -> [Point]
+neighbours b slippery (r, c) = [n | n <- allDirs, inBounds n, not (isWall n), passable n]
   where
     allDirs = [(r, c + 1), (r + 1, c), (r, c - 1), (r - 1, c)]
     inBounds (r', c') = r' >= 0 && r' < length b && c' >= 0 && c' < length (head b)
     isWall n = b `at` n == '#'
+    passable n = not slippery || b `at` n `notElem` "<>v^" || forcedNext (b `at` n) n /= (r, c)
 
-findNode :: Board -> Point -> Point -> Int -> Nodes -> (Point, Int)
-findNode b f p d ns
-  | p `elem` ns = (p, d)
-  | otherwise   = findNode b p nextP (d + 1) ns
+findNode :: Board -> Bool -> Point -> Point -> Int -> Nodes -> Maybe (Point, Int)
+findNode b slippery f p d ns
+  | p `elem` ns       = Just (p, d)
+  | not (null nextPs) = findNode b slippery p (head nextPs) (d + 1) ns
+  | otherwise         = Nothing
   where
     -- Assume there's always just one, otherwise would be in node
-    nextP = head $ [n | n <- simpleNeighbours b p, n /= f]
+    nextPs = [n | n <- neighbours b slippery p, n /= f]
 
-findNeighbourNodes :: Board -> Node -> Nodes -> [(Node, Int)]
-findNeighbourNodes b s ns = [findNode b s n 1 ns | n <- simpleNeighbours b s]
+findNeighbourNodes :: Board -> Bool -> Node -> Nodes -> [(Node, Int)]
+findNeighbourNodes b slippery s ns = catMaybes [findNode b slippery s n 1 ns | n <- neighbours b slippery s]
 
-buildGraph :: Board -> Nodes -> Graph
-buildGraph _ [] = M.empty
-buildGraph b ns = M.fromList [(n, findNeighbourNodes b n ns) | n <- ns]
+buildGraph :: Board -> Nodes -> Bool -> Graph
+buildGraph _ [] _ = M.empty
+buildGraph b ns slippery = M.fromList [(n, findNeighbourNodes b slippery n ns) | n <- ns]
 
 -- This is surprisingly slow given how small the graph is lol
 longestPath :: Graph -> Node -> Int -> Node -> Set Node -> Int
@@ -85,7 +68,8 @@ main =
     let grid = lines contents
     let start = (0, 1)
     let end = (length grid - 1, length (head grid) - 2)
-    print $ longest grid end (Empty:|>((-1, 0), start, 0))
     let nodes = getNodes grid start end
-    let graph = buildGraph grid nodes
-    print $ longestPath graph start 0 end St.empty
+    let graph1 = buildGraph grid nodes True
+    print $ longestPath graph1 start 0 end St.empty
+    let graph2 = buildGraph grid nodes False
+    print $ longestPath graph2 start 0 end St.empty
