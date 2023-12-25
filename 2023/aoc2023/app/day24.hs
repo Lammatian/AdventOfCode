7ee9c4f468f82a57ee9c086c5aa563edb9fe1015
Lammatian <mateusz.parafinski@gmail.com>
2023-12-25 17:02:37 +0100

    Add 2023 day 24 'part 2'
    Half was done on paper and in Python Repl lol

--- 2023/aoc2023/app/day24.hs
+++ 2023/aoc2023/app/day24.hs
@@ -1,10 +1,14 @@
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
 import Data.Maybe (isJust, fromJust)
+import Data.Map (Map, empty, insertWith, keys)
+import qualified Data.Map as M (filter, map)
 
 type Point = (Int, Int, Int)
 type Velo = (Int, Int, Int)
 type Hail = (Point, Velo)
+data Var = X | Y | Z
+type Remainders = Map Var (Map Int [Int])
 
 parseTuple :: String -> Point
 parseTuple t = (a, b, c)
@@ -55,6 +59,41 @@ isInFuture ((hx, hy, _), (dx, dy, _)) (x, y)
     hx' = fromIntegral hx
     hy' = fromIntegral hy
 
+-- Get (x, d_x), (y, d_y) or (z, d_z)
+getVar :: Var -> Hail -> (Int, Int)
+getVar X ((x, _, _), (dx, _, _)) = (x, dx)
+getVar Y ((_, y, _), (_, dy, _)) = (y, dy)
+getVar Z ((_, _, z), (_, _, dz)) = (z, dz)
+
+getRemainders :: Var -> [Hail] -> Map Int [Int]
+getRemainders _ [] = empty
+getRemainders v (h:hs) = insertWith (++) dvh [vh] (getRemainders v hs)
+  where
+    (vh, dvh) = getVar v h
+
+getDiffs :: [Int] -> [Int]
+getDiffs vs = [x - y | x <- vs, y <- vs, x > y]
+
+gcd' :: [Int] -> Int
+gcd' vs = foldl gcd (head vs) vs
+
+-- Extended GCD with Bezout
+eGCD :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer, Integer)
+eGCD (or', 0) (os, _) (ot, _) = (os, ot, or')
+eGCD (or', r) (os, s) (ot, t) = eGCD (r, or' - q * r) (s, os - q * s) (t, ot - q * t)
+  where
+    q = or' `div` r
+
+-- Given equations x = a_i (mod n_i) as form of pairs (a_i, n_i) return solution x = a (mod N) as a pair (a, N)
+extendedCRT :: [(Integer, Integer)] -> (Integer, Integer)
+extendedCRT [] = error "Empty system of equations"
+extendedCRT [(a, m)] = (a `mod` m, m)
+extendedCRT ((a, m):(b, n):es) = extendedCRT ((x `mod` md, md):es)
+  where
+    (u, v, g) = eGCD (m, n) (1, 0) (0, 1)
+    md = m * n `div` g
+    x = (a * v * n + b * u * m) `div` g
+
 main :: IO ()
 main =
   do
@@ -66,3 +105,31 @@ main =
     let intersections = [(h1, h2, fromJust i) | h1 <- hails, h2 <- hails, h1 /= h2, let i = intersection h1 h2, isJust i]
     let filteredIntersections = filter (\(h1, h2, i) -> withinBounds i lowerBound upperBound && isInFuture h1 i && isInFuture h2 i) intersections
     print $ length filteredIntersections `div` 2
+    -- This gets us for each d_v all vs and then calculates the (v_i - v_j) for each pair of vs
+    let niceXs = M.filter ((>= 3) . length) $ getRemainders X hails
+    let niceYs = M.filter ((>= 3) . length) $ getRemainders Y hails
+    let niceZs = M.filter ((>= 3) . length) $ getRemainders Z hails
+    -- This gets us the pairs (k, l) such that b_v - k divides l
+    -- where b_v is the coefficient of variable v in equation a_v + b_v*v
+    let gcdsX = M.map (gcd' . getDiffs) niceXs
+    let gcdsY = M.map (gcd' . getDiffs) niceYs
+    let gcdsZ = M.map (gcd' . getDiffs) niceZs
+    print gcdsX
+    print gcdsY
+    print gcdsZ
+    -- From the above we can figure out what the actual values for b_x, b_y and b_z are for the rock
+    let bx = 131 :: Int
+    let by = -259 :: Int
+    let bz = 102 :: Int
+    -- let bx = -3 :: Int
+    -- let by = 1 :: Int
+    -- let bz = 2 :: Int
+    -- Generate equations v = a (mod b_v - d_v) for all coordinates v
+    let eqsX = [(toInteger x, toInteger (bx - dx)) | ((x, _, _), (dx, _, _)) <- hails]
+    let eqsY = [(toInteger y, toInteger (by - dy)) | ((_, y, _), (_, dy, _)) <- hails]
+    let eqsZ = [(toInteger z, toInteger (bz - dz)) | ((_, _, z), (_, _, dz)) <- hails]
+    -- Solve these beauties
+    print $ extendedCRT eqsX
+    print $ extendedCRT eqsY
+    print $ extendedCRT eqsZ
+    -- Do some maths and get a solution lol
