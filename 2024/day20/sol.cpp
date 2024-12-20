f5b997655ea53794730512b17025512d71ea8678
Lammatian <mateusz.parafinski@gmail.com>
2024-12-20 08:35:18 +0100

    Speedify 2024 day 20
    With this one simple trick went to
    * ~0.1s with -O3 (down from ~2.2s)
    * ~1s without optimisations (down from ~20s)

--- 2024/day20/sol.cpp
+++ 2024/day20/sol.cpp
@@ -60,26 +60,8 @@ board<ll> distances_to_end(const board<char>& b, pos start, pos end) {
     return ds;
 }
 
-vector<ll> possible_savings(const board<char>& b, const board<ll>& d, pos p, int max_dist) {
-    set<pos> visited;
-    vector<ll> result;
-    queue<pair<pos, int>> q;
-    q.push({p, 0});
-    while (!q.empty()) {
-        auto [curr, dist] = q.front();
-        q.pop();
-        if (d[curr] >= 0 && d[curr] + dist < d[p]) {
-            result.push_back(d[p] - d[curr] - dist);
-        }
-        for (auto n: b.neighbours(curr)) {
-            if (visited.find(n) != visited.end()) continue;
-            if (dist == max_dist) continue;
-            visited.insert(n);
-            q.push({n, dist + 1});
-        }
-    }
-
-    return result;
+ll dist(pos p1, pos p2) {
+    return abs(p1.r - p2.r) + abs(p1.c - p2.c);
 }
 
 int main(int argc, char** argv) {
@@ -97,18 +79,15 @@ int main(int argc, char** argv) {
     b.maxr = b.size();
     b.maxc = b[0].size();
 
-    auto [time, path] = fastest_path(b, start, end);
-    board<ll> d = distances_to_end(b, start, end);
     ll result1 = 0;
     ll result2 = 0;
-    for (auto p: path) {
-        auto savings1 = possible_savings(b, d, p, 2);
-        auto savings2 = possible_savings(b, d, p, 20);
-        for (auto s: savings1) {
-            if (s >= min_savings) result1++;
-        }
-        for (auto s: savings2) {
-            if (s >= min_savings) result2++;
+    auto [time, path] = fastest_path(b, start, end);
+    board<ll> d = distances_to_end(b, start, end);
+    for (auto p1: path) {
+        for (auto p2: path) {
+            ll ds = dist(p1, p2);
+            if (ds <= 2 && d[p2] - d[p1] - ds >= min_savings) result1++;
+            if (ds <= 20 && d[p2] - d[p1] - ds >= min_savings) result2++;
         }
     }
     cout << result1 << "\n";
